704d50729aa6953e3f7fe79a45ab85d40b1c746d
hiru <rlawoehd187@naver.com>
2020-01-28 21:45:33 +0900

    msm8960-common: power HAL version 1.1 -> 1.0
    * my mistake.

--- msm8960.mk
+++ msm8960.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # Bluetooth
 PRODUCT_PACKAGES += \
