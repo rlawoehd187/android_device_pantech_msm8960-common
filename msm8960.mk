dac5b0b24be4cab157d7ba61e5304184652662ff
hiru <rlawoehd187@naver.com>
2020-01-29 15:23:17 +0900

    msm8960-common: rework power HAL

--- msm8960.mk
+++ msm8960.mk
@@ -132,7 +132,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
